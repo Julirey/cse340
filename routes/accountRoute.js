6d43aab1e37e9981b4e050d4832f9fa4fecb302c
Julirey <reyes.0271204@gmail.com>
2024-10-22 22:41:37 -0400

    Add w4 team activity

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -19,4 +19,25 @@ router.post(
   utilities.handleErrors(accountController.registerAccount)
 )
 
+// Process the login attempt
+router.post(
+  "/login",
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+  utilities.handleErrors(
+    // Temporary measure to allow testing 
+   async (req, res) => {
+    let nav = await utilities.getNav()
+    req.flash(
+      "notice",
+      `You've successfully logged in. Welcome!`
+    )
+    res.status(200).render("account/login", {
+      title: "Login",
+      nav,
+      errors: null
+    })
+  })
+)
+
 module.exports = router;
